bfdcb9d33ab88959af16ee03a21f769bbcdccfc8
matsumoto <matA0012>
2020-09-27 19:00:09 +0900

    setNavigationBar

--- lib/a920_android.dart
+++ lib/a920_android.dart
@@ -1,14 +1,17 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class A920Android {
-  static const MethodChannel _channel =
-      const MethodChannel('a920_android');
+  static const MethodChannel _channel = const MethodChannel('a920_android');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static Future<String> setNavigationBar() async {
+    final String version = await _channel.invokeMethod('setNavigationBar');
     return version;
   }
 }
